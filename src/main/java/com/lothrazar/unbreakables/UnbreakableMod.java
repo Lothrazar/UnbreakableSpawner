cea66d8660ab1f907bce54a1ae25e64f7ec2e929
lothrazar <samson.bassett@gmail.com>
2020-07-08 09:09:35 -0700

    1.16 update

--- src/main/java/com/lothrazar/unbreakables/UnbreakableMod.java
+++ src/main/java/com/lothrazar/unbreakables/UnbreakableMod.java
@@ -1,15 +1,13 @@
 package com.lothrazar.unbreakables;
 
 import java.lang.reflect.Field;
-import net.minecraft.block.Block;
+import net.minecraft.block.AbstractBlock.AbstractBlockState;
 import net.minecraft.block.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-// TODO: The value here should match an entry in the META-INF/mods.toml file
-// TODO: Also search and replace it in build.gradle
 @Mod(UnbreakableMod.MODID)
 public class UnbreakableMod {
 
@@ -22,10 +20,11 @@ public class UnbreakableMod {
 
   private void setup(final FMLCommonSetupEvent event) {
     //reflection
-    Field f = ObfuscationReflectionHelper.findField(Block.class, "field_149782_v");//blockHardness
+    Field f = ObfuscationReflectionHelper.findField(AbstractBlockState.class, "field_235705_i_");//hardness
     f.setAccessible(true);
     try {
-      f.set(Blocks.SPAWNER, -1);
+      //      Blocks.SPAWNER.getDefaultState().hardness = -1;
+      f.set(Blocks.SPAWNER.getDefaultState(), -1);
     }
     catch (IllegalArgumentException e) {
       e.printStackTrace();
